be3ac6b521e3f6844f60c79d2f74265bd38a36a9
Lazar <lazar@htldornbirn.at>
2023-12-11 12:17:41 +0100

    for schleife aufgabe 1

---
+++ javascript/übung 3/script.js
@@ -0,0 +1,13 @@
+let number = 30;
+
+if (number < 20) {
+    console.log("mini");
+}
+
+if (number >= 20 && number <= 50) {
+    console.log("Medium");
+}
+
+if (number > 50) {
+    console.log("large");
+}

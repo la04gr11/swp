dd351ec09deb728fd8adabcec6644a92bfe1948b
Lazar <lazar@htldornbirn.at>
2023-12-12 11:40:14 +0100

    if abfrage übung 2

--- javascript/übung 3/script.js
+++ javascript/übung 3/script.js
@@ -11,3 +11,17 @@ if (number >= 20 && number <= 50) {
 if (number > 50) {
     console.log("large");
 }
+
+let zahl = 100;
+
+if (zahl <= 7) {
+    console.log("mini");
+}
+
+if (zahl >= 7 && zahl <= 90) {
+    console.log("Medium");
+}
+
+if (zahl > 90) {
+    console.log("large");
+}

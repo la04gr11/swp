241f8232bd8e4085cc51c3583e21c61ad171a678
Lazar <lazar@htldornbirn.at>
2023-12-18 12:15:59 +0100

    strings und zahlen übung 2

--- javascript/übung 2.1/script.js
+++ javascript/übung 2.1/script.js
@@ -5,3 +5,7 @@ let ergebnis = erstezahl/zweitezahl
 
 console.log("The result of " + erstezahl + " divided by " + zweitezahl + " is " + ergebnis)
  
+const Firstname = "Lazar"
+const Lastname = "Grujic"
+
+console.log("ich heise " + Firstname +  Lastname)

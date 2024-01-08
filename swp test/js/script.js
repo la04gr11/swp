c4a937a3ac3c41c814233a0dde141e56a0707206
Lazar <lazar@htldornbirn.at>
2024-01-08 11:51:35 +0100

    Test 20240108 ase enter the commit message for your changes. Lines starting

---
+++ swp test/js/script.js
@@ -0,0 +1,11 @@
+let zahl = Math.floor(Math.random() * 16);
+
+if(zahl > 12){
+    console.log(zahl)
+}
+
+
+
+
+
+//Erstelle eine Schleife die jeweils eine Zufallszahl zwischen 5 und 15  generiert. Wenn die Zufallszahl größer als 12 ist gib die Zahl aus. Gib am Ende aus wieviele Zahlen kleiner-gleich 12 waren und wieviele größer als 12 waren  

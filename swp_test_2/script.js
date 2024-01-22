fbb065909d6570fd10e4c60bd6200adb159f4a9a
Lazar <lazar@htldornbirn.at>
2024-01-22 11:30:52 +0100

    swptest2

---
+++ swp_test_2/script.js
@@ -0,0 +1,14 @@
+let randomNumber = Math.floor(Math.random() * (51 - 35 + 1)) + 35;
+
+if((randomNumber<38)&&(randomNumber>36)){
+console.log(randomNumber)
+}
+
+if(randomNumber == 35){
+    console.log("ohhh")
+}
+
+if((randomNumber == 50)||(randomNumber == 51)){
+    console.log("jupidu")
+}
+
